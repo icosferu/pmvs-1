e138081ba65768f8057d48d7c248df4a3b6d1300
nikki <mirai@dao>
2015-10-05 17:14:41 +0300

    norm

--- run.sh
+++ run.sh
@@ -1,10 +1,2 @@
-source config.ini
-while :
-do
-wget -q -O - http://6.pogoda.by/ |
-iconv -f WINDOWS-1251 -t UTF-8 |
-tr '\n' ' ' |
-grep -Poa '<div id=\"content\">(.*?)C' |
-grep -Poa '[0-9]+\.[0-9]'
-sleep $sleeptime
-done
+. ./config.ini
+links2 -dump $pagename | grep -m 1 -Po '[\-\+][0-9]+...'
